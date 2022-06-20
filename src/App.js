6f38d3f5fb910f2e1161f2b9b0ba7e39b132713a
Ligalaiz <slavakuz88@gmail.com>
2022-06-20 22:13:57 +0300

    feat: S-5 implement slider and map

--- src/App.js
+++ src/App.js
@@ -79,5 +79,58 @@ const handleSubmit = (e) => {
   form.reset();
 };
 
-form.addEventListener('submit', handleSubmit);
+if (form) form.addEventListener('submit', handleSubmit);
 document.addEventListener('click', handleClick);
+
+// Slider
+const slider = document.querySelector('.slider');
+const prevBtn = document.querySelector('.slider__btn--prev');
+const nextBtn = document.querySelector('.slider__btn--next');
+const slides = slider.querySelectorAll('.slider__item');
+let slideIndex = 0;
+let isEnabled = true;
+
+const slideShow = (n) => {
+  slideIndex = (n + slides.length) % slides.length;
+};
+
+const hideItem = (direction) => {
+  isEnabled = false;
+  slides[slideIndex].classList.add(direction);
+  slides[slideIndex].addEventListener('animationend', function () {
+    this.classList.remove('active', direction);
+  });
+};
+
+const showItem = (direction) => {
+  slides[slideIndex].classList.add('next', direction);
+  slides[slideIndex].addEventListener('animationend', function () {
+    this.classList.remove('next', direction);
+    this.classList.add('active');
+    isEnabled = true;
+  });
+};
+
+const nextItem = (n) => {
+  hideItem('to-left');
+  slideShow(n + 1);
+  showItem('from-right');
+};
+
+const previousItem = (n) => {
+  hideItem('to-right');
+  slideShow(n - 1);
+  showItem('from-left');
+};
+
+prevBtn.addEventListener('click', () => {
+  if (isEnabled) {
+    previousItem(slideIndex);
+  }
+});
+
+nextBtn.addEventListener('click', () => {
+  if (isEnabled) {
+    nextItem(slideIndex);
+  }
+});
